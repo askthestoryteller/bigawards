1f165848adb283adb0739b16312970cbe6c6672a
askthestoryteller <138726260+askthestoryteller@users.noreply.github.com>
2023-09-11 00:27:52 +0200

    initial commit; added coming soon page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,79 @@
+<script type="module">
+    let test = 2024;
+</script>
+
+<div id="page">
+    <div id="content">
+        <center>
+            <div>
+                <img src="logo.png" alt="" />
+            </div>
+            <div id="video">
+                <iframe width='100%' style='aspect-ratio:16/9' 
+                    src="https://www.youtube.com/embed/xrQzHzlDPpo?si=kydy7HoP9Hr5BOXx"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                />
+            </div>
+            <div>
+                <p>
+                    Coming Soon -- Check back on October 2nd 2023 for our
+                    Launch!
+                </p>
+            </div>
+        </center>
+    </div>
+</div>
+
+<!-- 
+<button>Login with Twitch</button>
+
+<p>Don't have a Twitch?</p>
+
+<button>Login with Google</button>
+ -->
+
+<style>
+    * {
+        font-family: "brandon";
+        
+    }
+
+    
+    div#page {
+        background-image: url("bg3.png");
+        background-size: cover;
+ background-repeat: no-repeat;
+        color: #fffafb;
+    }
+
+    img{max-width: 80vw;}
+
+    div#video{
+position: relative;
+width: 50vw;
+    /* height: 0;
+padding-bottom: 56.25%; */
+
+  
+    }
+
+    /* iframe{ 
+        position:absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    } */
+
+    div#page {
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
